6e97c7864b472b47939c844c3df9f34fa30ebabb
Syntheticdev <35655841+Synthetic-Dev@users.noreply.github.com>
2022-06-30 16:50:24 +0100

    resource combining support

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Get environment variables
 require("dotenv").config()
 
+const Stream = require("stream")
 const AdmZip = require("adm-zip")
 const Express = require("express")
 const Timeout = require("connect-timeout")
@@ -39,45 +40,57 @@ app.get("/spigot-resource/:resource", (request, response, next) => {
     download(response, fileName)
 })
 
-// app.get("/spigot-resource-combine", (request, response, next) => {
-//     let resources = request.query.resources
-//     if (!resources) {
-//         return response.sendStatus(400)
-//     }
+app.get("/spigot-resource-combine", (request, response, next) => {
+    let resources = request.query.resources
+    if (!resources) {
+        return response.sendStatus(400)
+    }
 
-//     resources = resources.trim().split(",")
-//     let fileNames = []
-//     for (let resource of resources) {
-//         let fileName = RESOURCES_MAP[resource]
-//         if (!fileName) {
-//             return response.sendStatus(400)
-//         }
-//         fileNames.push(fileName)
-//     }
+    resources = resources.trim().split(",")
+    let fileNames = []
+    for (let resource of resources) {
+        let fileName = RESOURCES_MAP[resource]
+        if (!fileName) {
+            return response.sendStatus(400)
+        }
+        fileNames.push(fileName)
+    }
 
-//     if (fileNames.length < 2) {
-//         return response.sendStatus(400)
-//     }
+    if (fileNames.length < 2) {
+        return response.sendStatus(400)
+    }
 
-//     let zip = new AdmZip(`./resources/${fileNames[0]}.zip`)
-//     for (let i = 1; i < fileNames.length; i++) {
-//         let fileName = fileNames[i]
-//         let otherZip = new AdmZip(`./resources/${fileName}.zip`)
-//         for (let entry of otherZip.getEntries()) {
-//             let mainEntry = zip.getEntry(entry.entryName)
-//             if (mainEntry) {
-//                 if (entry.isDirectory) continue;
-//             } else {
-//                 if (entry.isDirectory) {
-//                     zip.addFile(entry.entryName)
-//                 } else {
-//                     zip.addFile(entry.entryName, entry.getData(), entry.comment, entry.attr)
-//                 }
-//             }
-//         }
-//     }
-//     response.end(zip.toBuffer())
-// })
+    let zip = new AdmZip(`./resources/${fileNames[0]}.zip`)
+
+    for (let i = 1; i < fileNames.length; i++) {
+        let fileName = fileNames[i]
+        let otherZip = new AdmZip(`./resources/${fileName}.zip`)
+        for (let entry of otherZip.getEntries()) {
+            let mainEntry = zip.getEntry(entry.entryName)
+            if (mainEntry) {
+                if (entry.isDirectory) continue;
+            } else {
+                if (entry.isDirectory) {
+                    zip.addFile(entry.entryName)
+                } else {
+                    zip.addFile(entry.entryName, entry.getData(), entry.comment, entry.attr)
+                }
+            }
+        }
+    }
+
+    let combinedName = fileNames.join("+")
+
+    let mcmeta = JSON.parse(zip.readAsText("pack.mcmeta"))
+    mcmeta.pack.description = combinedName
+    zip.updateFile("pack.mcmeta", Buffer.from(JSON.stringify(mcmeta)))
+
+    let readStream = new Stream.PassThrough()
+    readStream.end(zip.toBuffer())
+
+    response.set('Content-disposition', 'attachment; filename=' + combinedName + ".zip")
+    readStream.pipe(response)
+})
 
 /**
  * Listeners
