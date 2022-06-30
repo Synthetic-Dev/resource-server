4d6ed03d3087d8a68c34be550a5d68fe9efc04e5
Syntheticdev <35655841+Synthetic-Dev@users.noreply.github.com>
2022-06-30 14:19:06 +0100

    update

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 // Get environment variables
 require("dotenv").config()
 
+const AdmZip = require("adm-zip")
 const Express = require("express")
 const Timeout = require("connect-timeout")
 
 const RequestLogger = require("./utils/requestLogger")
+const { response } = require("express")
 
 /**
  * Startup
@@ -19,24 +21,63 @@ function download(response, fileName) {
     response.download(__dirname + `/resources/${fileName}.zip`, `${fileName}.zip`)
 }
 
+const RESOURCES = ["Netherite Shield", "Backpacks"]
+const RESOURCES_MAP = {}
+
+for (let resource of RESOURCES) {
+    RESOURCES_MAP[resource.toLowerCase().replace(/\s+/g, "-")] = resource
+}
+
 app.get("/spigot-resource/:resource", (request, response, next) => {
     let resource = request.params.resource;
-    let fileSent = false
 
-    switch (resource) {
-        case "netherite-shield":
-            download(response, "Netherite Shield")
-            fileSent = true
-            break
-        case "backpacks":
-            download(response, "Backpacks")
-            fileSent = true
+    let fileName = RESOURCES_MAP[resource];
+    if (!fileName) {
+        return response.sendStatus(404)
     }
 
-    if (!fileSent) {
-        response.sendStatus(404)
-    }
+    download(response, fileName)
 })
+
+// app.get("/spigot-resource-combine", (request, response, next) => {
+//     let resources = request.query.resources
+//     if (!resources) {
+//         return response.sendStatus(400)
+//     }
+
+//     resources = resources.trim().split(",")
+//     let fileNames = []
+//     for (let resource of resources) {
+//         let fileName = RESOURCES_MAP[resource]
+//         if (!fileName) {
+//             return response.sendStatus(400)
+//         }
+//         fileNames.push(fileName)
+//     }
+
+//     if (fileNames.length < 2) {
+//         return response.sendStatus(400)
+//     }
+
+//     let zip = new AdmZip(`./resources/${fileNames[0]}.zip`)
+//     for (let i = 1; i < fileNames.length; i++) {
+//         let fileName = fileNames[i]
+//         let otherZip = new AdmZip(`./resources/${fileName}.zip`)
+//         for (let entry of otherZip.getEntries()) {
+//             let mainEntry = zip.getEntry(entry.entryName)
+//             if (mainEntry) {
+//                 if (entry.isDirectory) continue;
+//             } else {
+//                 if (entry.isDirectory) {
+//                     zip.addFile(entry.entryName)
+//                 } else {
+//                     zip.addFile(entry.entryName, entry.getData(), entry.comment, entry.attr)
+//                 }
+//             }
+//         }
+//     }
+//     response.end(zip.toBuffer())
+// })
 
 /**
  * Listeners
